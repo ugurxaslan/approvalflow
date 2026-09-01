7ed6de6f072542fc60e27d67216c5f4d1827fb47
ugurxaslan <ugurxaslan@gmail.com>
2026-09-01 18:41:42 +0300

    initial commit

---
+++ src/main/java/com/ugurxaslan/approvalflow/ApprovalflowApplication.java
@@ -0,0 +1,13 @@
+package com.ugurxaslan.approvalflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprovalflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ugurxaslan/approvalflow/ApprovalflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugurxaslan.approvalflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
