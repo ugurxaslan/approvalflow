d2f892d012f3d70e345a16b9cf09e49807011395
ugurxaslan <ugurxaslan@gmail.com>
2026-09-11 12:19:11 +0300

    feat(dto): add specific request DTOs with validation rules

---
+++ src/main/java/com/ugurxaslan/approvalflow/model/dto/LeaveRequestDTO.java
@@ -0,0 +1,20 @@
+package com.ugurxaslan.approvalflow.model.dto;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class LeaveRequestDTO extends BaseRequestDTO{
+    @NotNull(message = "Start date is required")
+    @FutureOrPresent(message = "Start date cannot be in the past")
+    private LocalDate startDate;
+
+    @NotNull(message = "End date is required")
+    @FutureOrPresent(message = "End date cannot be in the past")
+    private LocalDate endDate;
+}
